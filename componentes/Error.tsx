1329f7410993039fd445457b48fe66b8a6004010
Gyananjay Nayak <gyananjay.n@innomick.com>
2022-10-12 19:30:01 +0530

    added next Image component

--- componentes/Error.tsx
+++ componentes/Error.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import { Container } from 'reactstrap'
 import styles from '../styles/Error.module.css'
 
 export const Error = (): JSX.Element => {
   return (
     <Container className={styles.error}>
-      <img
+      <Image
         data-testid="error-image"
         src="/cross-img.gif"
         alt="error-img"
